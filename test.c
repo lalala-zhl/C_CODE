99b702fa0b41c0123b62e06074844d6acbb25181
lalala-zhl <116262620+lalala-zhl@users.noreply.github.com>
2022-11-21 23:36:34 +0800

    Add files via upload
    输入10个整数，求出最大值

---
+++ test.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+	int arr[10];                  
+	int i = 0;                    //i控制下标
+	int max = 0;
+	
+	for (i = 0; i < 10; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+
+	max = arr[0];                 //把小标为0的元素赋给max
+
+	for (i = 0; i < 10; i++)
+	{
+		if (arr[i] > max)         //如果此元素大于max
+		{
+			max = arr[i];         //把此元素赋给max
+		}
+	}
+
+	printf("max=%d\n", max);
+
+	return 0;
+}
